d83d4b5ee1fe90c09d77919fc3adcecf52f8f9cf
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-18 16:04:59 -0700

    users can edit & update reviews

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 mongoose.connect('mongodb://localhost/kiki');
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 const Show = mongoose.model('Show', {
   title: String,
@@ -21,6 +24,7 @@ let shows = [
   { title: "RuPaul's Drag Race", genre: "Reality TV"}
 ]
 
+// INDEX
 app.get('/', (req, res) => {
     Show.find()
     .then(shows => {
@@ -28,10 +32,12 @@ app.get('/', (req, res) => {
 
 })
 
+// NEW
 app.get('/shows/new', (req, res) => {
     res.render('shows-new', {})
 })
 
+// CREATE
 app.post('/shows', (req, res) => {
   Show.create(req.body).then((show) => {
       res.redirect(`/shows/${show._id}`)
@@ -40,6 +46,7 @@ app.post('/shows', (req, res) => {
   })
 })
 
+// SHOW
 app.get('/shows/:id', (req, res) => {
     Show.findById(req.params.id).then((show) => {
         res.render('shows-show', {show: show})
@@ -47,6 +54,24 @@ app.get('/shows/:id', (req, res) => {
         console.log(err.message)
     })
 })
+
+// EDIT
+app.get('/shows/:id/edit', (req, res) => {
+  Show.findById(req.params.id, function(err, show) {
+    res.render('shows-edit', {show: show});
+  })
+})
+
+// UPDATE
+app.put('/shows/:id', (req, res) => {
+    Show.findByIdAndUpdate(req.params.id, req.body)
+        .then(show => {
+            res.redirect(`/shows/${show._id}`)
+        }).catch(err => {
+            console.log(err.message)
+        })
+})
+// DELETE
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
