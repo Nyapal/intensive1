7f7e3986430cc9769bb6d569a8165607f53f1c70
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-18 11:42:59 -0700

    added mongodb & shows model

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/kiki');
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
+const Show = mongoose.model('Show', {
+  title: String,
+  genre: String
+});
+
 let shows = [
   { title: "Shameless", genre: "Comedy" },
-  { title: "Game of Thrones", genre: "Drama" }
+  { title: "Game of Thrones", genre: "Drama" },
+  { title: "The Real Housewives of Atlanta", genre: ["Comedy", " Reality-TV"] },
+  { title: "RuPaul's Drag Race", genre: "Reality TV"}
 ]
 
+// app.get('/', (req, res) => {
+//   Review.find()
+//     .then(reviews => {
+//         res.render('reviews-index', { reviews: reviews });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+// })
+
 app.get('/', (req, res) => {
-  res.render('shows-index', { shows: shows });
+    Show.find()
+    .then(shows => {
+        res.render('shows-index', { shows: shows });
+    })
+
 })
 
 app.listen(3000, () => {
