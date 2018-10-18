9ea27f65a7d8a4b4c8e1c7076d46c18c331d7504
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-18 11:23:06 -0700

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+var exphbs = require('express-handlebars');
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
