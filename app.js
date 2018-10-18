5d45a741be056c94eb9861c334b09a1798f8a669
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-18 11:36:08 -0700

    users can see all shows

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
-var exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
+let shows = [
+  { title: "Shameless", genre: "Comedy" },
+  { title: "Game of Thrones", genre: "Drama" }
+]
+
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('shows-index', { shows: shows });
 })
 
 app.listen(3000, () => {
